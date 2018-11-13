0491dee6c15dab2f8242823eb25dcd71074081d4
azr3 <registera13@gmail.com>
2018-11-13 13:39:37 -0500

    add is_tachycardic

---
+++ hr_sentinel_server.py
@@ -0,0 +1,47 @@
+
+
+def is_tachycardic(age, heart_rate):
+    """
+    Determine if heart_rate is tachycardia based on the person's age
+    used https://en.wikipedia.org/wiki/Tachycardia
+    :param age: age in integer
+    :param heart_rate: person's heart rate in BPM ex(160)
+    :return: True or False
+    """
+    if age < 1:
+        if heart_rate > 169:
+            return True
+        else:
+            return False
+    elif age <= 2:
+        if heart_rate > 151:
+            return True
+        else:
+            return False
+    elif age <= 4:
+        if heart_rate > 137:
+            return True
+        else:
+            return False
+    elif age <= 7:
+        if heart_rate > 133:
+            return True
+        else:
+            return False
+    elif age <= 11:
+        if heart_rate > 130:
+            return True
+        else:
+            return False
+    elif age <= 15:
+        if heart_rate > 119:
+            return True
+        else:
+            return False
+    elif age > 15:
+        if heart_rate > 100:
+            return True
+        else:
+            return False
+
+
