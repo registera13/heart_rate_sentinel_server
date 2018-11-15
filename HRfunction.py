937afada22b6f1f2af924bde15e75bbcc3981b23
azr3 <registera13@gmail.com>
2018-11-15 13:44:52 -0500

    add sendgrid

--- HRfunction.py
+++ HRfunction.py
@@ -1,5 +1,6 @@
-import os
 import sendgrid
+import os
+from sendgrid.helpers.mail import *
 import datetime
 from flask import Flask, jsonify, request
 from pymodm import connect
@@ -110,5 +111,17 @@ def is_tachycardic(age, heart_rate):
             return True
         else:
             return False
+
+def send_sendgrid():
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email("test@example.com")
+    to_email = Email("test@example.com")
+    subject = "Sending with SendGrid is Fun"
+    content = Content("text/plain", "and easy to do anywhere, even with Python")
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
 
 if __name__ == "__main__":
